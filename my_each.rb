3bc54f029a785ee1e167e0f06d3ff6244f82cb0e
Jmacklin1 <Macklin.Jason@gmail.com>
2020-10-12 11:09:09 -0400

    done

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,10 @@
 def my_each(array)
-  if block_given?
     i = 0
     while i < array.length
+      yield(array[i])
+      i+=1
     end
     array
   # put argument(s) here
 end
 
-my_each([1,2,3,4]) do |num| 
-  return num
-end
-end
